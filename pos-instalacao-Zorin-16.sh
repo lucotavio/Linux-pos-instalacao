980ea4188f55bf9a274eb1db0f5e3c9e01f4d6d4
Luciano <luc.oliveira343@gmail.com>
2021-10-15 17:49:17 -0300

    Adicionando a instalacao dda webcam

--- pos-instalacao-Zorin-16.sh
+++ pos-instalacao-Zorin-16.sh
@@ -655,11 +655,23 @@ fi
 
 
 ## Instalando ADB
+##sudo apt-get update -y
+##sudo apt-get install -y adb
+##sudo apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+##sudo apt install -y qemu-kvm
+##sudo adduser $USER kvm
+
+
+## Instalando webcam
 sudo apt-get update -y
-sudo apt-get install -y adb
-sudo apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
-sudo apt install -y qemu-kvm
-sudo adduser $USER kvm
+sudo apt install linux-headers-`uname -r` gcc make
+sudo ./install-video
+cd /tmp/
+wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_1.8.0.zip
+unzip droidcam_latest.zip -d droidcam
+cd droidcam && sudo ./install-client
+
+cd  /home/$USER/Downloads/Programas/
 
 
 
