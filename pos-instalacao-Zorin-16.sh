98a319f975a152633d1bea93adbb8e127f576e40
Luciano <luc.otavio@gmail.com>
2021-10-16 23:24:58 -0300

    consertando a instalacao do fortclient

--- pos-instalacao-Zorin-16.sh
+++ pos-instalacao-Zorin-16.sh
@@ -588,8 +588,9 @@ sudo apt-get install libgconf-2-4
 sudo apt-get install -y vlc
 
 ##Instalando FortiClient
+wget -O - https://repo.fortinet.com/repo/7.0/ubuntu/DEB-GPG-KEY | sudo apt-key add -
 echo "deb [arch=amd64] https://repo.fortinet.com/repo/7.0/ubuntu/ /bionic multiverse" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
+sudo apt-get update -y
 sudo apt install forticlient
 
 ## Instalando e configurando Meld
