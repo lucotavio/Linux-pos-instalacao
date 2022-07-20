d9a06e099a8c7d0932233755e2e71ed948871c88
Luciano <luc.otavio@gmail.com>
2022-07-20 19:27:52 -0300

    alterando

--- mysql-Ubuntu-22-04.sh
+++ mysql-Ubuntu-22-04.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+
 echo "Digite a senha atual: "
 read senhaAtual
 
 echo "Digite a senha nova: "
 read senhaNova
+
 sudo mysql -u root "-p${senhaAtual}" -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by '${senhaNova}';"
+sudo mysql_secure_installation
