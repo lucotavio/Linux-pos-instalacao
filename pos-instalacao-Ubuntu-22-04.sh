89a128ac56f76d3c4977b6e515c3bd5e06260aea
Luciano <luc.otavio@gmail.com>
2022-07-20 13:56:00 -0300

    alterando

--- pos-instalacao-Ubuntu-22-04.sh
+++ pos-instalacao-Ubuntu-22-04.sh
@@ -636,66 +636,6 @@ echo "**************************************************************************
 
 
 
-
-echo -e "\n\n\n************************************************** INSTALANDO  MARIADB  OU  MYSQL ************************************************"
-
-cd /home/$USER/Downloads/Programas/
-while true
-do
-    echo -e "\n\nDigite opção 1 ou 2"
-    echo "1 - MariaDB"
-    echo "2 - MySql"
-
-    read opcao
-
-    if [ $opcao -eq 1 ];then
-        sudo apt-get install software-properties-common dirmngr apt-transport-https
-        sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-        sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirror.ufro.cl/mariadb/repo/10.5/ubuntu 2 main'
-        sudo apt update -y
-        sudo apt install -y mariadb-server
-        sudo systemctl start mariadb222222+
-        sudo systemctl enable mariadb
-        sudo mysql_secure_installation
-
-        ## Dando ao usuario root todas as permissoes no SGBD MariaDB
-        sudo mysql -u root "-p789789" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '789789';"
-
-        ## Criando ususario luciano
-        sudo mysql -u root "-p789789" -e "CREATE USER 'luciano'@'localhost' IDENTIFIED BY '789789';"
-
-        ## Dando ao usuario luciano todas as permissoes no SGBD MariaDB
-        sudo mysql -u root "-p789789" -e "GRANT ALL PRIVILEGES ON *.* TO 'luciano'@localhost IDENTIFIED BY '789789';"
-
-        echo -e "\n\n******************** FIM  DA  INSTALACAO  DO  MARIADB ********************"
-        break
-    elif [ $opcao -eq 2 ];then
-        echo -e "\n\n******************** INSTALANDO  O  MYSQL ********************"
-
-        echo -e "\n\n\Mysql - 36 KB"
-        if [ -e mysql-apt-config_0.8.22-1_all.deb ];then
-            echo "O mysql-apt-config_0.8.22-1_all.deb ja existe"
-        else
-            curl -L -o mysql-apt-config_0.8.22-1_all.deb https://www.dropbox.com/s/2mh0c7by1gagm4c/mysql-apt-config_0.8.22-1_all.deb?dl=0
-        fi
-
-
-        sudo dpkg -i mysql-apt-config_0.8.22-1_all.deb
-        sudo apt --fix-broken install -y
-        sudo apt-get update -y
-        sudo apt-get install mysql-server
-
-        echo -e "\n\n******************** FIM  DA  INSTALACAO  DO   MYSQL********************"
-        break
-    else
-        echo -e "\n\nOpcao invalida"
-    fi
-done
-
-echo "********************************************************************************************************************"
-
-
-
 while true
 do
     echo -e "\n\nDigite opção 1 ou 2"
@@ -798,3 +738,54 @@ sudo update-alternatives --config java
 sudo apt install flatpak
 sudo apt install gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+
+
+echo -e "\n\n\n************************************************** INSTALANDO  MARIADB  OU  MYSQL ************************************************"
+
+cd /home/$USER/Downloads/Programas/
+while true
+do
+    echo -e "\n\nDigite opção 1 ou 2"
+    echo "1 - MariaDB"
+    echo "2 - MySql"
+
+    read opcao
+
+    if [ $opcao -eq 1 ];then
+        sudo apt-get install software-properties-common dirmngr apt-transport-https
+        sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+        sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirror.ufro.cl/mariadb/repo/10.5/ubuntu 2 main'
+        sudo apt update -y
+        sudo apt install -y mariadb-server
+        sudo systemctl start mariadb222222+
+        sudo systemctl enable mariadb
+        sudo mysql_secure_installation
+
+        ## Dando ao usuario root todas as permissoes no SGBD MariaDB
+        sudo mysql -u root "-p789789" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '789789';"
+
+        ## Criando ususario luciano
+        sudo mysql -u root "-p789789" -e "CREATE USER 'luciano'@'localhost' IDENTIFIED BY '789789';"
+
+        ## Dando ao usuario luciano todas as permissoes no SGBD MariaDB
+        sudo mysql -u root "-p789789" -e "GRANT ALL PRIVILEGES ON *.* TO 'luciano'@localhost IDENTIFIED BY '789789';"
+
+        echo -e "\n\n******************** FIM  DA  INSTALACAO  DO  MARIADB ********************"
+        break
+    elif [ $opcao -eq 2 ];then
+        echo -e "\n\n******************** INSTALANDO  O  MYSQL ********************"
+
+        sudo apt update -y
+        sudo apt install mysql-server
+        systemctl is-active mysql
+        sudo mysql_secure_installation
+        
+        echo -e "\n\n******************** CONTINUA NO SCRIPT mysql-Ubuntu-22.04.sh********************"
+        break
+    else
+        echo -e "\n\nOpcao invalida"
+    fi
+done
+
+echo "********************************************************************************************************************"
